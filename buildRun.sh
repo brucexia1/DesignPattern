ec4196184f062bdb5490c056abcacb99314aff6c
brucexia1 <bruce_xia@yeah.net>
2016-09-20 21:52:08 +0800

    modify build shell
    Signed-off-by: brucexia1 <bruce_xia@yeah.net>

--- buildRun.sh
+++ buildRun.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cmake .
-make -j4 1>succ.txt 2>fail.txt
-cat succ.txt
-cat fail.txt
+make -j4
+#make -j4 1>succ.txt 2>fail.txt
+#cat succ.txt
+#cat fail.txt
 
 cd build/
 ./DesignPattern
