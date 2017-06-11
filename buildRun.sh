9e2a5b1d325b82593ed045b3fcba7381e7500966
xia <bruce_xia@yeah.net>
2017-06-11 19:46:57 +0800

    add shell

--- buildRun.sh
+++ buildRun.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+basepath=$(cd `dirname $0`; pwd)
 
-cmake .
+
+#build 3rdlib
+cd basepath/3rdlib/gtest/buildUnix
+if [ ! -f "gtest.a" ]; then
+echo "aaa"
+fi
+
+
+#build this prj
+cd basepath/build/
+cmake ../
 make -j4
 #make -j4 1>succ.txt 2>fail.txt
-#cat succ.txt
-#cat fail.txt
 
-cd build/
 ./DesignPattern
-
 
